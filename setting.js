94a3846e9e0f6e71ff79c729de7e4e92c9a4c086
user <borishuai@augmentum.com.cn>
2014-07-01 10:41:59 +0800

    remove qiniu config

--- setting.js
+++ setting.js
@@ -1,6 +1,6 @@
 module.exports = {
-  accessKey: 'azdM9LzCScaBNQolFRs38r5txPx6_jj49TNbFvJ8',
-  secretKey: '9722TOZmbMl5wCBYgzAdDQ2ENAEDoI4-a1P9m_Mh',
-  bucket: 'bh-requirejs-example',
-  cdnDomain: 'http://bh-requirejs-example.qiniudn.com',
+  accessKey: 'xxxx',
+  secretKey: 'xxxx',
+  bucket: 'xxxx',
+  cdnDomain: 'http://xxxx.qiniudn.com',
 };
